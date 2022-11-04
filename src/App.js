f3b4b24b10d33c0e3df74102e7a0294c9c6c20ab
mcombs289 <maddy.combs4@gmail.com>
2022-11-04 02:09:00 -0400

    dark mode, button hover, sticky nav, prof pic

--- src/App.js
+++ src/App.js
@@ -1,129 +1,161 @@
 import React, { useState } from "react";
 import "./index.css";
 import "./dark.css";
+import NavigationBar from "./NavigationBar";
+import darkModeProf from "./images/darkModeProf.png";
+import lightModeProf from "./images/lightModeProf.png";
+import Keepsake from "./images/keepsake.png";
+import Boba from "./images/boba.png";
+import TowerOfHanoi from "./images/TowerOfHanoi.png";
+import resume from "./images/Madison-Combs-Resume.pdf";
 import { motion } from "framer-motion";
 
 const App = () => {
   const [isOn, setIsOn] = useState(false);
-  const toggleSwitch = () => {
-    setIsOn(!isOn);
-  };
-
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30,
-  };
 
   return (
-    <>
-      <div className="switch" data-ison={isOn} onClick={toggleSwitch}>
-        <motion.div className="handle" layout transition={spring} />
-      </div>
-      <header class="main-header">
-        <h1>
-          <span>Madison Combs</span>
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-          quam!
-        </p>
-      </header>
+    <div className={isOn ? "dark-profile-body" : "profile-body"}>
+      <NavigationBar isOn={isOn} setIsOn={setIsOn} />
+      <div className="push-down">
+        <header class={isOn ? "dark-main-header" : "main-header"}>
+          {isOn ? (
+            <img className="profile-picture" src={darkModeProf} alt="profpic" />
+          ) : (
+            <img
+              className="profile-picture"
+              src={lightModeProf}
+              alt="profpic"
+            />
+          )}
+          <br />
+          <h1>
+            <span>Madison Combs</span>
+          </h1>
+          <a href={resume} target="_blank" rel="noreferrer">
+            Open Second PDF
+          </a>
+        </header>
 
-      <main class="container">
-        <section className={isOn ? "card" : "dark-card"}>
-          <img src="https://i.ibb.co/0VywMkW/paint-3.png" alt="" />
-          <div>
-            <h3>Keepsake</h3>
-            <p>
-              A social media app meant to house general media reviews and
-              recommendations. While there are apps out there specialized to
-              specific forms of media, such as IMDB for movies and Goodreads for
-              books, Keepsake provides a centralized place for you to share your
-              thoughts and keep your friends updated on what you are watching
-              and reading.
-            </p>
-            <br />
-            <p>
-              <b>Technologies:</b> Javascript, React V6, React-Redux, Node,
-              Bootstrap, Express, Sequelize.
-            </p>
-            <div className="button-row">
-              <a
-                href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
-                class="btn"
-              >
-                Website
-              </a>
-              <a
-                href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
-                class="btn"
-              >
-                GitHub
-              </a>
-              <a
-                href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
-                class="btn"
-              >
-                YouTube
-              </a>
+        <main class="container">
+          <section className={isOn ? "dark-card" : "card"}>
+            <img src={Keepsake} alt="" />
+            <div>
+              <h2>Keepsake</h2>
+              <p>
+                A social media app meant to house general media reviews and
+                recommendations. While there are apps out there specialized to
+                specific forms of media, such as IMDB for movies and Goodreads
+                for books, Keepsake provides a centralized place for you to
+                share your thoughts and keep your friends updated on what you
+                are watching and reading.
+              </p>
+              <br />
+              <p>
+                <b>Technologies:</b> Javascript, React V6, React-Redux, Node,
+                Bootstrap, Express, Sequelize.
+              </p>
+              <div className="button-row">
+                <motion.a
+                  whileHover={{
+                    scale: [1, 1.1, 1],
+                    transition: {
+                      duration: 1.5,
+                      repeat: "Infinity",
+                      repeatType: "loop",
+                    },
+                  }}
+                  href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
+                  class={isOn ? "dark-btn" : "btn"}
+                >
+                  Website
+                </motion.a>
+                <motion.a
+                  whileHover={{
+                    scale: [1, 1.1, 1],
+                    transition: {
+                      duration: 1.5,
+                      repeat: "Infinity",
+                      repeatType: "loop",
+                    },
+                  }}
+                  href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
+                  class={isOn ? "dark-btn" : "btn"}
+                >
+                  GitHub
+                </motion.a>
+                <motion.a
+                  whileHover={{
+                    scale: [1, 1.1, 1],
+                    transition: {
+                      duration: 1.5,
+                      repeat: "Infinity",
+                      repeatType: "loop",
+                    },
+                  }}
+                  href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
+                  class={isOn ? "dark-btn" : "btn"}
+                >
+                  YouTube
+                </motion.a>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section className={isOn ? "card" : "dark-card"} data-aos="fade-left">
-          <img src="https://i.ibb.co/0VywMkW/paint-3.png" alt="" />
-          <div>
-            <h3>Acrylic Painting Two</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-              error enim laudantium, animi veniam libero eveniet culpa unde
-              perferendis illo fugit corporis, voluptatibus totam dolorum,
-              maiores magnam officia. Ab, delectus.
-            </p>
-          </div>
-        </section>
+          <section className={isOn ? "dark-card" : "card"} data-aos="fade-left">
+            <img src={Boba} alt="" />
+            <div>
+              <h2>Acrylic Painting Two</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                error enim laudantium, animi veniam libero eveniet culpa unde
+                perferendis illo fugit corporis, voluptatibus totam dolorum,
+                maiores magnam officia. Ab, delectus.
+              </p>
+              <div className="button-row">
+                <motion.a
+                  whileHover={{
+                    scale: [1, 1.1, 1],
+                    transition: {
+                      duration: 1.5,
+                      repeat: "Infinity",
+                      repeatType: "loop",
+                    },
+                  }}
+                  href="https://www.youtube.com/watch?v=jQnAIYv9v08&list=PLx0iOsdUOUmnMB_t8aLcctAYN-lXgZBUM&index=4"
+                  class={isOn ? "dark-btn" : "btn"}
+                >
+                  GitHub
+                </motion.a>
+              </div>
+            </div>
+          </section>
 
-        <section className={isOn ? "card" : "dark-card"} data-aos="fade-right">
-          <img src="https://i.ibb.co/0VywMkW/paint-3.png" alt="" />
-          <div>
-            <h3>Acrylic Painting Three</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-              error enim laudantium, animi veniam libero eveniet culpa unde
-              perferendis illo fugit corporis, voluptatibus totam dolorum,
-              maiores magnam officia. Ab, delectus.
-            </p>
-          </div>
-        </section>
-
-        <section className={isOn ? "card" : "dark-card"} data-aos="fade-left">
-          <img src="https://i.ibb.co/5LkJFRP/paint-4.png" alt="" />
-          <div>
-            <h3>Acrylic Painting Four</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-              error enim laudantium, animi veniam libero eveniet culpa unde
-              perferendis illo fugit corporis, voluptatibus totam dolorum,
-              maiores magnam officia. Ab, delectus.
-            </p>
-          </div>
-        </section>
-
-        <section className={isOn ? "card" : "dark-card"} data-aos="fade-right">
-          <img src="https://i.ibb.co/61R8Q2y/paint-5.png" alt="" />
-          <div>
-            <h3>Acrylic Painting Five</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-              error enim laudantium, animi veniam libero eveniet culpa unde
-              perferendis illo fugit corporis, voluptatibus totam dolorum,
-              maiores magnam officia. Ab, delectus.
-            </p>
-          </div>
-        </section>
-      </main>
-    </>
+          <section
+            className={isOn ? "dark-card" : "card"}
+            data-aos="fade-right"
+          >
+            <img src={TowerOfHanoi} alt="" />
+            <div>
+              <h2>Acrylic Painting Three</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                error enim laudantium, animi veniam libero eveniet culpa unde
+                perferendis illo fugit corporis, voluptatibus totam dolorum,
+                maiores magnam officia. Ab, delectus.
+              </p>
+              <div className="button-row">
+                <a
+                  href="https://codepen.io/mcombs7/details/yrZpRK"
+                  class={isOn ? "dark-btn" : "btn"}
+                >
+                  CodePen
+                </a>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
   );
 };
 
---
+++ src/NavigationBar.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Link } from "react-bootstrap";
+import { motion } from "framer-motion";
+
+const NavigationBar = ({ isOn, setIsOn }) => {
+  const [fixed, setFixed] = useState(false);
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setFixed(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <Navbar
+      className={`${fixed ? "sticky" : ""} ${isOn ? "dark-navbar" : "navbar"}`}
+    >
+      <div className="switch" data-ison={isOn} onClick={toggleSwitch}>
+        <motion.div className="handle" layout transition={spring} />
+      </div>
+      <p style={{ fontSize: "30px", marginLeft: "1rem", marginTop: "1rem" }}>
+        &#127767;
+      </p>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
