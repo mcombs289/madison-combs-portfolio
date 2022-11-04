9bf8405c128888168369ce390c7c2a0657e52aea
mcombs289 <maddy.combs4@gmail.com>
2022-11-04 02:32:25 -0400

    superficial changes

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -29,9 +29,15 @@ const NavigationBar = ({ isOn, setIsOn }) => {
       <div className="switch" data-ison={isOn} onClick={toggleSwitch}>
         <motion.div className="handle" layout transition={spring} />
       </div>
-      <p style={{ fontSize: "30px", marginLeft: "1rem", marginTop: "1rem" }}>
-        &#127767;
-      </p>
+      {isOn ? (
+        <p style={{ fontSize: "30px", marginLeft: "1rem", marginTop: "1rem" }}>
+          &#127769;
+        </p>
+      ) : (
+        <p style={{ fontSize: "40px", marginLeft: "1rem", marginTop: ".6rem" }}>
+          &#127774;
+        </p>
+      )}
     </Navbar>
   );
 };
